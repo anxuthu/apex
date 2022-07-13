a1db5b6c6f04b86a15fea52655ffe397dbc7c7f3
anxuthu <annn.xu@gmail.com>
2022-07-13 10:38:31 -0400

    Update utils.py

--- apex/amp/utils.py
+++ apex/amp/utils.py
@@ -9,7 +9,10 @@ def is_cuda_enabled():
     return torch.version.cuda is not None
 
 def get_cuda_version():
-    return tuple(int(x) for x in torch.version.cuda.split('.'))
+    if torch.version.cuda is not None:
+        return tuple(int(x) for x in torch.version.cuda.split('.'))
+    else:
+        return None
 
 def is_fp_tensor(x):
     if is_nested(x):
